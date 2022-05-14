88337132489509ffdda24a5606f5804b8a2f0c6a
ahsanumair1199 <ahsanumair1199@gmail.com>
2022-05-14 13:41:11 -0700

    staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zd0f&6+%9033mdc@%n)k51)7lx0^&0c25q0wh&vulzcrsfol^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -173,7 +173,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 django_heroku.settings(locals())
 # Default primary key field type
